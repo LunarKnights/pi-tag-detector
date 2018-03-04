e844b4ee1f464db62ab532ec264eeec579216c14
Kelvin Ly <kelvin.ly1618@gmail.com>
2018-03-03 23:46:53 -0500

    Implement node TODO test

--- pitag_ros.cpp
+++ pitag_ros.cpp
@@ -1,13 +1,18 @@
 #include "ros/ros.h"
 
+#include <nodelet/loader.h>
+
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "pitag_ros");
 
-  auto nh = ros::NodeHandle();
-  auto nhPrivate = ros::NodeHandle("~");
-
-  // TODO: spawn nodelet
+  // spawn nodelet
+  nodelet::Loader nodelet;
+  nodelet::M_string remap(ros::names::getRemappings());
+  nodelet::V_string nargv;
+  nodelet.load(ros::this_node::getName(),
+          "pigtag_ros/PitagNodelet",
+          remap, nargv);
 
   ros::spin();
   return 0;
